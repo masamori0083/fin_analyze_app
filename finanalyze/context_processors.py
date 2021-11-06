3fff88dc93bbbd74b58f839e82aa9661e8e0aebb
masmaori0083 <masmaori0083@gmail.com>
2021-11-06 14:13:37 +0900

    first commit

---
+++ finanalyze/context_processors.py
@@ -0,0 +1,11 @@
+from .models import Company
+
+
+def company_list(request):
+    company_list = Company.objects.all()
+
+    params = {
+        'company_list': company_list,
+    }
+
+    return params
